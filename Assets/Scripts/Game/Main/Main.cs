cec8f1701a1ef9ad30eb7e3a79fc81d4d3925684
Bartolomeus <63509206+patyczak232323@users.noreply.github.com>
2026-09-18 14:53:14 +0200

    Default experimental engine options to off

--- Assets/Scripts/Game/Main/Main.cs
+++ Assets/Scripts/Game/Main/Main.cs
@@ -86,6 +86,8 @@ namespace DLS.Game
 				Prefs_SimTargetStepsPerSecond = 1000,
 				Prefs_SimStepsPerClockTick = 250,
 				Prefs_SimPaused = false,
+				Prefs_ExperimentalNativeCMode = 0,
+				Prefs_ExperimentalEngineDiagnostics = false,
 				AllCustomChipNames = Array.Empty<string>(),
 				StarredList = BuiltinCollectionCreator.GetDefaultStarredList().ToList(),
 				ChipCollections = new List<ChipCollection>(BuiltinCollectionCreator.CreateDefaultChipCollections())
