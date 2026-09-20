c518983d31573d55b8ffce3a6eb9c4d2ad3c3d91
Bartolomeus <63509206+patyczak232323@users.noreply.github.com>
2026-09-20 21:09:58 +0200

    Save subchip mirror state

--- Assets/Scripts/SaveSystem/DescriptionCreator.cs
+++ Assets/Scripts/SaveSystem/DescriptionCreator.cs
@@ -65,7 +65,9 @@ namespace DLS.SaveSystem
 				subChip.Position,
 				// Don't save colour info for bus since it changes based on received input, so would just trigger unnecessary 'unsaved changes' warnings
 				subChip.IsBus ? null : subChip.OutputPins.Select(p => new OutputPinColourInfo(p.Colour, p.Address.PinID)).ToArray(),
-				(uint[])subChip.InternalData?.Clone()
+				(uint[])subChip.InternalData?.Clone(),
+				subChip.MirrorX,
+				subChip.MirrorY
 			);
 		}
 
