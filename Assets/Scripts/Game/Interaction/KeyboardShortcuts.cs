28c3e94fe0338597c2e71cdc26d74af514854ddc
Bartolomeus <63509206+patyczak232323@users.noreply.github.com>
2026-09-20 21:08:43 +0200

    Route editor commands through configurable shortcuts

--- Assets/Scripts/Game/Interaction/KeyboardShortcuts.cs
+++ Assets/Scripts/Game/Interaction/KeyboardShortcuts.cs
@@ -1,3 +1,4 @@
+using DLS.Description;
 using Seb.Helpers;
 using UnityEngine;
 
@@ -6,50 +7,88 @@ namespace DLS.Game
 	public static class KeyboardShortcuts
 	{
 		// ---- Main Menu shortcuts
-		public static bool MainMenu_NewProjectShortcutTriggered => CtrlShortcutTriggered(KeyCode.N);
-		public static bool MainMenu_OpenProjectShortcutTriggered => CtrlShortcutTriggered(KeyCode.O);
-		public static bool MainMenu_SettingsShortcutTriggered => CtrlShortcutTriggered(KeyCode.S);
-		public static bool MainMenu_QuitShortcutTriggered => CtrlShortcutTriggered(KeyCode.Q);
+		public static bool MainMenu_NewProjectShortcutTriggered => Triggered(ShortcutAction.MainMenuNewProject);
+		public static bool MainMenu_OpenProjectShortcutTriggered => Triggered(ShortcutAction.MainMenuOpenProject);
+		public static bool MainMenu_SettingsShortcutTriggered => Triggered(ShortcutAction.MainMenuSettings);
+		public static bool MainMenu_QuitShortcutTriggered => Triggered(ShortcutAction.MainMenuQuit);
 
 		// ---- Bottom Bar Menu shortcuts ----
-		public static bool SaveShortcutTriggered => CtrlShortcutTriggered(KeyCode.S);
-		public static bool LibraryShortcutTriggered => CtrlShortcutTriggered(KeyCode.L);
-		public static bool PreferencesShortcutTriggered => CtrlShortcutTriggered(KeyCode.P);
-		public static bool CreateNewChipShortcutTriggered => CtrlShortcutTriggered(KeyCode.N);
-		public static bool QuitToMainMenuShortcutTriggered => CtrlShortcutTriggered(KeyCode.Q);
-		public static bool SearchShortcutTriggered => CtrlShortcutTriggered(KeyCode.F);
+		public static bool SaveShortcutTriggered => Triggered(ShortcutAction.Save);
+		public static bool LibraryShortcutTriggered => Triggered(ShortcutAction.ChipLibrary);
+		public static bool PreferencesShortcutTriggered => Triggered(ShortcutAction.Preferences);
+		public static bool CreateNewChipShortcutTriggered => Triggered(ShortcutAction.CreateNewChip);
+		public static bool QuitToMainMenuShortcutTriggered => Triggered(ShortcutAction.QuitToMainMenu);
+		public static bool SearchShortcutTriggered => Triggered(ShortcutAction.Search);
 
+		// ---- Editing shortcuts ----
+		public static bool DuplicateShortcutTriggered => Triggered(ShortcutAction.Duplicate);
+		public static bool ToggleGridShortcutTriggered => Triggered(ShortcutAction.ToggleGrid);
+		public static bool ResetCameraShortcutTriggered => Triggered(ShortcutAction.ResetCamera);
+		public static bool UndoShortcutTriggered => Triggered(ShortcutAction.Undo);
+		public static bool RedoShortcutTriggered => Triggered(ShortcutAction.Redo);
+		public static bool MirrorHorizontalShortcutTriggered => Triggered(ShortcutAction.MirrorHorizontal);
+		public static bool MirrorVerticalShortcutTriggered => Triggered(ShortcutAction.MirrorVertical);
 
-		// ---- Misc shortcuts ----
-		public static bool DuplicateShortcutTriggered => MultiModeHeld && InputHelper.IsKeyDownThisFrame(KeyCode.D);
-		public static bool ToggleGridShortcutTriggered => CtrlShortcutTriggered(KeyCode.G);
-		public static bool ResetCameraShortcutTriggered => CtrlShortcutTriggered(KeyCode.R);
-		public static bool UndoShortcutTriggered => CtrlShortcutTriggered(KeyCode.Z);
-		public static bool RedoShortcutTriggered => CtrlShiftShortcutTriggered(KeyCode.Z);
-
-		// ---- Single key shortcuts ----
+		// ---- Single key / simulation shortcuts ----
+		// Cancel/confirm/delete remain fixed safety/UI controls.
 		public static bool CancelShortcutTriggered => InputHelper.IsKeyDownThisFrame(KeyCode.Escape);
 		public static bool ConfirmShortcutTriggered => InputHelper.IsKeyDownThisFrame(KeyCode.Return) || InputHelper.IsKeyDownThisFrame(KeyCode.KeypadEnter);
 		public static bool DeleteShortcutTriggered => InputHelper.IsKeyDownThisFrame(KeyCode.Backspace) || InputHelper.IsKeyDownThisFrame(KeyCode.Delete);
-		public static bool SimNextStepShortcutTriggered => InputHelper.IsKeyDownThisFrame(KeyCode.Space) && !InputHelper.CtrlIsHeld;
-		public static bool SimPauseToggleShortcutTriggered => CtrlShortcutTriggered(KeyCode.Space);
+		public static bool SimNextStepShortcutTriggered => Triggered(ShortcutAction.SimulationNextStep);
+		public static bool SimPauseToggleShortcutTriggered => Triggered(ShortcutAction.SimulationPause);
 
 		// ---- Dev shortcuts ----
 		public static bool OpenSaveDataFolderShortcutTriggered => InputHelper.IsKeyDownThisFrame(KeyCode.O) && InputHelper.CtrlIsHeld && InputHelper.ShiftIsHeld && InputHelper.AltIsHeld;
 
 		// ---- Modifiers ----
 		public static bool SnapModeHeld => InputHelper.CtrlIsHeld;
-
-		// In "Multi-mode", placed chips will be duplicated once placed to allow placing again; selecting a chip will add it to the current selection; etc.
 		public static bool MultiModeHeld => InputHelper.AltIsHeld || InputHelper.ShiftIsHeld;
 		public static bool StraightLineModeHeld => InputHelper.ShiftIsHeld;
 		public static bool StraightLineModeTriggered => InputHelper.IsKeyDownThisFrame(KeyCode.LeftShift);
 		public static bool CameraActionKeyHeld => InputHelper.AltIsHeld;
 		public static bool TakeFirstFromCollectionModifierHeld => InputHelper.CtrlIsHeld || InputHelper.AltIsHeld || InputHelper.ShiftIsHeld;
 
-		// ---- Helpers ----
-		static bool CtrlShortcutTriggered(KeyCode key) => InputHelper.IsKeyDownThisFrame(key) && InputHelper.CtrlIsHeld && !(InputHelper.AltIsHeld || InputHelper.ShiftIsHeld);
-		static bool CtrlShiftShortcutTriggered(KeyCode key) => InputHelper.IsKeyDownThisFrame(key) && InputHelper.CtrlIsHeld && InputHelper.ShiftIsHeld && !(InputHelper.AltIsHeld);
-		static bool ShiftShortcutTriggered(KeyCode key) => InputHelper.IsKeyDownThisFrame(key) && InputHelper.ShiftIsHeld && !(InputHelper.AltIsHeld || InputHelper.CtrlIsHeld);
+		public static ShortcutBinding GetBinding(ShortcutAction action)
+		{
+			AppSettings settings = Main.ActiveAppSettings;
+			return settings.GetBinding(action);
+		}
+
+		public static string GetBindingDisplayString(ShortcutAction action) => GetBinding(action).ToDisplayString();
+
+		public static string GetActionDisplayName(ShortcutAction action) =>
+			action switch
+			{
+				ShortcutAction.MainMenuNewProject => "New Project",
+				ShortcutAction.MainMenuOpenProject => "Open Project",
+				ShortcutAction.MainMenuSettings => "Settings",
+				ShortcutAction.MainMenuQuit => "Quit",
+				ShortcutAction.Save => "Save",
+				ShortcutAction.ChipLibrary => "Chip Library",
+				ShortcutAction.Preferences => "Preferences",
+				ShortcutAction.CreateNewChip => "Create New Chip",
+				ShortcutAction.QuitToMainMenu => "Quit To Main Menu",
+				ShortcutAction.Search => "Search",
+				ShortcutAction.Duplicate => "Duplicate",
+				ShortcutAction.ToggleGrid => "Toggle Grid",
+				ShortcutAction.ResetCamera => "Reset Camera",
+				ShortcutAction.Undo => "Undo",
+				ShortcutAction.Redo => "Redo",
+				ShortcutAction.SimulationNextStep => "Simulation Step",
+				ShortcutAction.SimulationPause => "Pause / Resume Simulation",
+				ShortcutAction.MirrorHorizontal => "Mirror Horizontal",
+				ShortcutAction.MirrorVertical => "Mirror Vertical",
+				_ => action.ToString()
+			};
+
+		static bool Triggered(ShortcutAction action)
+		{
+			ShortcutBinding binding = GetBinding(action);
+			if (!binding.IsBound || !InputHelper.IsKeyDownThisFrame(binding.Key)) return false;
+
+			return InputHelper.CtrlIsHeld == binding.Ctrl &&
+			       InputHelper.ShiftIsHeld == binding.Shift &&
+			       InputHelper.AltIsHeld == binding.Alt;
+		}
 	}
 }
