abc2f896b1812187af7b344ae05659cb3099f966
Bartolomeus <63509206+patyczak232323@users.noreply.github.com>
2026-09-22 00:20:08 +0200

    Remove legacy simulator dev RAM primitive

--- Assets/Scripts/Simulation/Simulator.cs
+++ Assets/Scripts/Simulation/Simulator.cs
@@ -520,40 +520,6 @@ namespace DLS.Simulation
 
 					break;
 				}
-				case ChipType.dev_Ram_8Bit:
-				{
-					uint addressPin = chip.InputPins[0].State;
-					uint dataPin = chip.InputPins[1].State;
-					uint writeEnablePin = chip.InputPins[2].State;
-					uint resetPin = chip.InputPins[3].State;
-					uint clockPin = chip.InputPins[4].State;
-
-					// Detect clock rising edge
-					bool clockHigh = PinState.FirstBitHigh(clockPin);
-					bool isRisingEdge = clockHigh && chip.InternalState[^1] == 0;
-					chip.InternalState[^1] = clockHigh ? 1u : 0;
-
-					// Write/Reset on rising edge
-					if (isRisingEdge)
-					{
-						if (PinState.FirstBitHigh(resetPin))
-						{
-							for (int i = 0; i < 256; i++)
-							{
-								chip.InternalState[i] = 0;
-							}
-						}
-						else if (PinState.FirstBitHigh(writeEnablePin))
-						{
-							chip.InternalState[GetAddress8Bit(addressPin)] = (uint)(PinState.GetBitStates(dataPin) & Address8BitMask);
-						}
-					}
-
-					// Output data at current address
-					chip.OutputPins[0].State = (ushort)chip.InternalState[GetAddress8Bit(addressPin)];
-
-					break;
-				}
 				case ChipType.Rom_256x16:
 				{
 					const int ByteMask = 0b11111111;
