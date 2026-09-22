365be7d33aafa45b098df708f2aa73ad17838a86
Bartolomeus <63509206+patyczak232323@users.noreply.github.com>
2026-09-22 10:50:37 +0200

    Increase Android touch target size

--- Assets/Scripts/Seb/Helpers/Input/MobileInputBridge.cs
+++ Assets/Scripts/Seb/Helpers/Input/MobileInputBridge.cs
@@ -22,7 +22,7 @@ namespace Seb.Helpers.InputHandling
 		public static bool RuntimeEnabled { get; private set; }
 		public static MobileKeyboardMode RequestedMode => requestedMode;
 		public static bool KeyboardVisible => RuntimeEnabled && !dismissed && Time.frameCount - lastFocusedFrame <= 1;
-		public static float TouchHitScale => RuntimeEnabled ? 1.35f : 1f;
+		public static float TouchHitScale => RuntimeEnabled ? 1.55f : 1f;
 
 		public static void EnableRuntime(bool enabled)
 		{
